1e920c6f769433679d823639ec1b19d41d5b5802
khaaledashraaf <250846853+khaaledashraaf@users.noreply.github.com>
2026-03-22 19:31:41 +0200

    added some stuff

--- src/components/site-chrome.tsx
+++ src/components/site-chrome.tsx
@@ -16,7 +16,7 @@ export function SiteChrome({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-[calc(100vh-3.5rem)] flex-col">
       {!isAbout && <AsciiBackground />}
       <main
-        className="relative z-10 mx-auto flex-1 flex flex-col max-w-4xl px-6 bg-transparent"
+        className="relative z-10 mx-auto flex-1 flex flex-col w-full max-w-4xl px-6 bg-transparent"
       >
         {children}
       </main>
